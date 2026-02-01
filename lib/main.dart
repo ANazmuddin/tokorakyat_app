1ec2a7cd992f9c42637748406dba0b232779fef9
ANazmuddin <anazmuddin072@gmail.com>
2026-02-01 20:38:58 +0700

    update logic main

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart'; // 1. Import Firebase
-import 'package:toko_rakyat/firebase_options.dart'; // 2. Import Options yang baru dibuat
+import 'package:firebase_core/firebase_core.dart'; 
+import 'package:toko_rakyat/firebase_options.dart'; 
 import 'package:toko_rakyat/controllers/cart_controller.dart';
-import 'package:toko_rakyat/views/login_page.dart';
-import 'package:toko_rakyat/controllers/auth_controller.dart';
+import 'package:toko_rakyat/controllers/auth_controller.dart'; 
+import 'package:toko_rakyat/views/splash_page.dart'; 
 
-// 3. Ubah main() jadi async
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // Wajib ada jika main() async
+  WidgetsFlutterBinding.ensureInitialized();
 
-  // 4. Inisialisasi Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -38,7 +36,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
       ),
-      home: const LoginPage(),
+      home: const SplashPage(),
     );
   }
 }
